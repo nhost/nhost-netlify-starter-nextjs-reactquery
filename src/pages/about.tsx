6bcd5f8ec28bce5d7b90bc3f98506dd9940453d5
Guido Curcio <guidomaurocurcio@gmail.com>
2022-07-29 13:10:41 -0300

    fix: add repository to the about page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,14 +8,18 @@ const About = () => {
         <Header />
         <div className="flex flex-col max-w-2xl mx-auto my-12 text-center">
           <h1 className="text-dim text-2xl font-semibold leading-none text-center">
-            NextJS and Nhost Starter
+            Nhost Netlify Starter Template
           </h1>
           <p className="pt-2 text-center">
-            This is an example project using Vue.js and Nhost to create a simple
-            conference management application.
+            This is an example front-end for the multi-conference example app
+            created with Nhost. It uses Next.js, React-Query, Tailwind, Nhost as
+            the backend (authentication, Postgres Database, GraphQL API) and
+            Netlify to host the front-end.
           </p>
           <div className="flex flex-col mt-10">
-            <a>https://github.com/nhost/vue-netlify-starter</a>
+            <a>
+              https://github.com/nhost/nhost-netlify-starter-nextjs-reactquery
+            </a>
           </div>
         </div>
       </div>
