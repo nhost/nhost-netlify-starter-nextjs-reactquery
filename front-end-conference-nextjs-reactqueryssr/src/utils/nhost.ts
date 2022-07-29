104920dc952c9316d0a492432a08626958795e43
Guido Curcio <guidomaurocurcio@gmail.com>
2022-07-29 06:27:41 -0300

    fix: add envs to sdk client.

--- front-end-conference-nextjs-reactqueryssr/src/utils/nhost.ts
+++ front-end-conference-nextjs-reactqueryssr/src/utils/nhost.ts
@@ -1,7 +1,8 @@
 import { NhostClient } from '@nhost/nextjs';
 
 const nhost = new NhostClient({
-  subdomain: 'localhost:1337',
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || '',
 });
 
 export { nhost };
