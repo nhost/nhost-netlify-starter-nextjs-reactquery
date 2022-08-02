caf845639263e5c64d71f97a6c7ecde0cfdec6df
Guido Curcio <guidomaurocurcio@gmail.com>
2022-08-01 21:25:05 -0300

    feat: speakers component and ui common folder.

---
+++ src/components/speakers/Speakers.tsx
@@ -0,0 +1,44 @@
+import { Loader } from '@/components/common/Loader';
+import { Speaker } from '@/components/speakers/Speaker';
+import { useSpeakersQuery } from '@/utils/__generated__/graphql';
+
+export const Speakers = () => {
+  const { data, isLoading, isError } = useSpeakersQuery();
+
+  if (isError) {
+    return (
+      <div className="bg-opacity-10 w-full max-w-xl px-4 py-4 mx-auto text-sm bg-red-900 rounded-md">
+        <h1 className="pb-2 text-xl font-medium leading-none text-center text-white">
+          Error:
+        </h1>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="flex mx-auto">
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid w-full grid-cols-4 gap-6">
+      {data.speakers.map((speaker) => {
+        return (
+          <Speaker
+            key={speaker.id}
+            id={speaker.id}
+            avatarUrl={
+              speaker.avatar_url || 'https://via.placeholder.com/350x350'
+            }
+            name={speaker.name}
+            social={speaker.social}
+            job={speaker.job_description}
+          />
+        );
+      })}
+    </div>
+  );
+};
--- src/pages/speakers.tsx
+++ src/pages/speakers.tsx
@@ -1,11 +1,10 @@
 import { Header } from '@/components/Header';
 import Layout from '@/components/Layout';
-import { Speaker } from '@/components/speakers/Speaker';
 import { AddNewSpeaker } from '@/components/speakers/AddNewSpeaker';
+import { Speakers } from '@/components/speakers/Speakers';
 import { useSpeakersQuery } from '@/utils/__generated__/graphql';
 import { useAuthenticated } from '@nhost/react';
 import { dehydrate, QueryClient } from '@tanstack/react-query';
-import { Loader } from '@/components/conferences/FeaturedConference';
 
 const SpeakersPage = () => {
   const isAuthenticated = useAuthenticated();
@@ -15,7 +14,7 @@ const SpeakersPage = () => {
       <div className=" text-white">
         <Header />
         <div className="flex flex-col max-w-5xl mx-auto my-10">
-          <SpeakersComponent />
+          <Speakers />
           {isAuthenticated ? (
             <div className="w-full max-w-lg py-10 mx-auto">
               <h1 className="text-dim pb-8 text-3xl font-medium leading-none text-center">
@@ -27,47 +26,6 @@ const SpeakersPage = () => {
         </div>
       </div>
     </Layout>
-  );
-};
-
-export const SpeakersComponent = () => {
-  const { data, isLoading, isError } = useSpeakersQuery();
-
-  if (isError) {
-    return (
-      <div className="bg-opacity-10 w-full max-w-xl px-4 py-4 mx-auto text-sm bg-red-900 rounded-md">
-        <h1 className="pb-2 text-xl font-medium leading-none text-center text-white">
-          Error:
-        </h1>
-      </div>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <div className="flex mx-auto">
-        <Loader />
-      </div>
-    );
-  }
-
-  return (
-    <div className="grid w-full grid-cols-4 gap-6">
-      {data.speakers.map((speaker) => {
-        return (
-          <Speaker
-            key={speaker.id}
-            id={speaker.id}
-            avatarUrl={
-              speaker.avatar_url || 'https://via.placeholder.com/350x350'
-            }
-            name={speaker.name}
-            social={speaker.social}
-            job={speaker.job_description}
-          />
-        );
-      })}
-    </div>
   );
 };
 
