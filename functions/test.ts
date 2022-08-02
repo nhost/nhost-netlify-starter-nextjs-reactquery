065df10f5826052a6d07bcf635336f053f480e33
Guido Curcio <guidomaurocurcio@gmail.com>
2022-08-02 11:08:45 -0300

    Update test.ts

--- functions/test.ts
+++ functions/test.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export default (req: Request, res: Response) => {
   res.setHeader("my-header", "123");
-  res.status(400).send("test");
+  res.status(200).send("test2");
 };
